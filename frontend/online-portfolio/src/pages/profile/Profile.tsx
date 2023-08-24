879a6dfed5668fbae5250535eec3e2f45617d132
unknown <tinashechitamba@outlook.com>
2023-08-24 22:40:51 +0200

    axios

--- frontend/online-portfolio/src/pages/profile/Profile.tsx
+++ frontend/online-portfolio/src/pages/profile/Profile.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react';
-import { Badge, Button, Col, Container, Row, Stack, Modal, Form } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NameAndLinks from './components/Name-and-links-component';
 import AboutAndExperience from './components/About-and-experience-componenets';
+import axios from 'axios';
+import { Portfolio } from '../../types/Portfolio';
+import { useState } from 'react';
+import { json } from 'stream/consumers';
+
 
 function Profile() {
+
+    const [portfolio , setPortfolio] = useState<Portfolio>();
+
+    axios
+    .get<Portfolio>('http://localhost:8080/portfolio')
+    .then(response => {
+      setPortfolio(response.data)
+    });
+
     return (
         <div>
             <Container fluid className='w-75'>
--- portfolioBuilder/src/main/java/com/mukundi/portfolioBuilder/controller/PortfolioController.java
+++ portfolioBuilder/src/main/java/com/mukundi/portfolioBuilder/controller/PortfolioController.java
@@ -1,12 +1,14 @@
 package com.mukundi.portfolioBuilder.controller;
 
 import com.mukundi.portfolioBuilder.domain.Portfolio;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("portfolio")
+@CrossOrigin("*")
 public class PortfolioController {
 
   @GetMapping("")
