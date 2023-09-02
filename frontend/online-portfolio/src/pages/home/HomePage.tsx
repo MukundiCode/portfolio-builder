ca83974a832c18549370c52ead4bb6a3204afc75
unknown <tinashechitamba@outlook.com>
2023-09-02 21:23:28 +0200

    nice animation

--- frontend/online-portfolio/src/pages/home/HomePage.tsx
+++ frontend/online-portfolio/src/pages/home/HomePage.tsx
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Container, Form, InputGroup, Row } from "react-bootstrap";
-import { Redirect } from "react-router-dom";
+import { useTypewriter } from 'react-simple-typewriter';
 
 function HomePage() {
 
     const [username, setUsername] = useState<string>();
+    const [textBoxPlaceHolder, setTextBoxPlaceHolder] = useTypewriter({
+        words: ["yourusername"],
+        loop: 0
+    })
 
-    const handleUsernameSubmit = () => { 
+    const handleUsernameSubmit = () => {
         console.log("Clicked")
-        axios.post('http://localhost:8080/portfolio/add', {username: username})
+        axios.post('http://localhost:8080/portfolio/add', { username: username })
             .then(response => {
                 console.log(response.data)
                 window.location.href = `/${username}`
@@ -17,25 +21,26 @@ function HomePage() {
     }
 
     return (
-        <div className="h-100">
-            <Container className="w-75 align-middle">
+        <div>
+            <Container className="w-50 mt-5 pt-5">
                 <Row>
-                    <div className="display-2 mb-3 name-font " >
+                    <div className="display-2 mb-3 home-font text-center" >
                         Your developer portfolio is one click away!
                     </div>
                 </Row>
-                <Row className="w-50 align-middle">
-                    <div>
-                        <InputGroup className="mb-3">
+                <Row className="d-flex justify-content-center">
+                    <div className="w-75 align-middle">
+                        <InputGroup className="mb-3 align-middle ">
                             <InputGroup.Text id="basic-addon3">
                                 devportfolio.me/
                             </InputGroup.Text>
                             <Form.Control
                                 id="basic-url"
                                 aria-describedby="basic-addon3"
+                                placeholder={textBoxPlaceHolder}
                                 onChange={(event) => setUsername(event.target.value)} />
+                            <Button onClick={handleUsernameSubmit} variant="dark">Launch</Button>
                         </InputGroup>
-                        <Button onClick={handleUsernameSubmit}>Launch</Button>
                     </div>
                 </Row>
             </Container>
