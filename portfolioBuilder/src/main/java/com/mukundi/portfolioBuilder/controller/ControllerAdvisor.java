67d2564c5dc1e6379302c5f879a1a69bf588077c
unknown <tinashechitamba@outlook.com>
2023-09-04 21:18:20 +0200

    controller advisor

---
+++ portfolioBuilder/src/main/java/com/mukundi/portfolioBuilder/controller/ControllerAdvisor.java
@@ -0,0 +1,51 @@
+package com.mukundi.portfolioBuilder.controller;
+
+import com.mukundi.portfolioBuilder.service.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ControllerAdvisor extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpHeaders headers,
+                                                                HttpStatus status, WebRequest request) {
+
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", Timestamp.valueOf(LocalDateTime.now()));
+    body.put("status", status.value());
+
+    Map<String, String> errors = exception.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+    body.put("errors", errors);
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(value = {EntityNotFoundException.class})
+  public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException exception, WebRequest request){
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", Timestamp.valueOf(LocalDateTime.now()));
+    body.put("status", HttpStatus.NOT_FOUND);
+
+    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+  }
+
+
+
+}
